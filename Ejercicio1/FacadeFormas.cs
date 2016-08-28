c9b503863a4441f32faa6377ebe3eb007f582611
MauriChamorro <mauricio-chamorro@hotmail.com>
2016-08-28 01:23:11 -0300

    Clases y Fachada

---
+++ Ejercicio1/FacadeFormas.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio1
+{
+    class FacadeFormas //Fachada (Facade)
+    {
+
+        private Punto iPunto;
+        private Circulo iCirculo;
+        private Triangulo iTriangulo;
+
+        public void DistanciaEntreDosPuntos()
+        {
+            Console.WriteLine("Punto 1");
+            Punto iPunto1 = this.CrearPunto();
+
+            Console.WriteLine("Punto 2");
+            Punto iPunto2 = this.CrearPunto();
+
+            Console.WriteLine("Resultado: " + this.Distancia(iPunto1,iPunto2));
+        }
+
+        public double Distancia(Punto pPunto1, Punto pPunto2)
+        {
+            return pPunto1.CalcularDistanciaDesde(pPunto2);
+        }
+
+        public Punto CrearPunto()
+        {
+            Console.WriteLine("Ingrese valor X");
+            double iX = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Ingrese valor Y");
+            double iY = Convert.ToDouble(Console.ReadLine());
+            iPunto = new Punto(iX, iY);
+            return iPunto;
+        }
+
+        public void AreaYPerimetroCirculo()
+        {
+            Console.WriteLine("1-Ingresar Centro y Radio");
+            Console.WriteLine("2-Ingresar X e Y, más el Radio");
+            byte iOpcion = Convert.ToByte(Console.ReadLine());
+
+            if (iOpcion == 1)
+            {
+                Console.WriteLine("Definiendo centro como Punto...");
+                Punto iPunto = this.CrearPunto();
+                Console.WriteLine("Ingresar valor de Radio");
+                double iRadio = Convert.ToDouble(Console.ReadLine());
+
+                iCirculo = new Circulo(iPunto, iRadio);
+                Console.WriteLine("El valor del AREA es: " + Math.Truncate(iCirculo.Area));
+                Console.WriteLine("El valor del PERIMETRO es: " + Math.Truncate(iCirculo.Perimetro));
+            }
+            else
+            {//opcion 2
+                Console.WriteLine("Definiendo centro directamente...");
+
+                Console.WriteLine("Ingresar valor X");
+                double iX = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Ingresar valor Y");
+                double iY = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Ingresar valor de Radio");
+                double iRadio = Convert.ToDouble(Console.ReadLine());
+
+                iCirculo = new Circulo(iX, iY, iRadio);
+
+                Console.WriteLine("El valor del AREA es: " + iCirculo.Area);
+                Console.WriteLine("El valor del PERIMETRO es: " + iCirculo.Perimetro);
+            }
+        }
+
+        public void AreaYPerimetroTriangulo()
+        {
+            Console.WriteLine("1-Deberá ingresar tres puntos");
+
+            Console.WriteLine("Punto 1");
+            Punto iPunto1 = this.CrearPunto();
+            Console.WriteLine("Punto 2");
+            Punto iPunto2 = this.CrearPunto();
+            Console.WriteLine("Punto 3");
+            Punto iPunto3 = this.CrearPunto();
+
+            iTriangulo = new Triangulo(iPunto1, iPunto2, iPunto3);
+            Console.WriteLine("El valor del AREA es: " + iTriangulo.Area);
+            Console.WriteLine("El valor del PERIMETRO es: " + iTriangulo.Perimetro);
+
+        }
+    }
+}
--- Ejercicio1/Program.cs
+++ Ejercicio1/Program.cs
@@ -10,6 +10,53 @@ namespace Ejercicio1
     {
         static void Main(string[] args)
         {
-        }
-    }
+            Console.WriteLine("AREAS Y PERIMETROS");
+            Console.WriteLine("Eliga figura geométrica:");
+            Console.WriteLine("1- Calcular distancia entre dos puntos");
+            Console.WriteLine("2- Círculo");
+            Console.WriteLine("3- Triángulo");
+            FacadeFormas iFFormas = new FacadeFormas(); //fachada
+
+            byte iOpcion = Convert.ToByte(Console.ReadLine());
+            
+            switch (iOpcion)
+            {
+                case 1:
+                    {
+                        //distancia entre dos puntos............
+                        Console.WriteLine("Distancia estre dos Puntos");
+                        iFFormas.DistanciaEntreDosPuntos();
+                        Console.ReadKey();
+                        
+                        break;
+                    }
+                case 2: //Circulo---------------------------------------
+                    {
+                        Console.WriteLine("Eligió Círculo...");
+                        iFFormas.AreaYPerimetroCirculo();
+                        Console.ReadKey();
+
+                        break;
+                    }
+                case 3://Triangulo---------------------------------------
+                    {
+                        
+                        Console.WriteLine("Eligió Triangulo...");
+                        iFFormas.AreaYPerimetroTriangulo();
+                        Console.ReadKey();
+
+                        break;
+                    }
+
+                default:
+                    {
+                        Console.WriteLine("Eliga una opcion correcta");
+                        Console.ReadKey();
+                        break;
+                    }
+            }
+       }
+    }     
 }
+
+
